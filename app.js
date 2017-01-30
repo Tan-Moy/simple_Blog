907abd3752819ebe681266b6d445d1eb37e64ade
Tan Moy <tanmoy.pto@gmail.com>
2017-01-31 02:37:40 +0530

    Installed ejs

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
+//Initialize
 var express = require("express");
-var app = express();
+app = express();
 bodyParser = require("body-parser");
 mongoose = require("mongoose");
 
+//Config
 mongoose.connect("mongodb://localhost/restful_blog_app");
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//Schema + model
 var blogSchema = mongoose.Schema({
-    title:String,
-    image:String,
-    body:String,
-    created:{type: Date, default: Date.now}
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+var Blog = mongoose.model("Blog", blogSchema);
+Blog.create({
+    title: "Test Blog",
+    image: "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FaZlqiAT.png&f=1",
+    body: "Hello There!!"
+});
+
+//RESTful Routes
+app.get("/blogs",function(req,res){
+    res.render('index');
 });
 
 
@@ -21,7 +35,7 @@ var blogSchema = mongoose.Schema({
 
 
 
-
-app.listen(3000,function(){
+//Server
+app.listen(8000, function () {
     console.log("serving...");
 });
